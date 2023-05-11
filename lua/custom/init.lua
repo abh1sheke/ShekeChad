be70d31622b3b84a9091592a59f04d26da03d211
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-05-11 17:14:09 +0530

    override vim defaults

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,34 @@
--- local autocmd = vim.api.nvim_create_autocmd
+vim.opt.guicursor = ""
 
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 4
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append "@-@"
+
+local function open_nvim_tree()
+  vim.cmd.NvimTreeToggle()
+end
+
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree } )
