72cff7bcd4ffef3b2d816fa1fb0b58b30b6854a6
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-15 14:10:44 +0530

    enable netrw and other im defaults

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,29 +1,39 @@
----@type ChadrcConfig
+---ty@pe ChadrcConfig
 local M = {}
 
 -- Path to overriding theme and highlights files
 local highlights = require "custom.highlights"
 
+M.lazy_nvim = {
+  performance = {
+    rtp = {
+      disabled_plugins = vim.tbl_filter(function(name)
+        return string.sub(name, 1, 5) ~= "netrw"
+      end, require("plugins.configs.lazy_nvim").performance.rtp.disabled_plugins),
+    },
+  },
+}
+
 M.ui = {
-  theme = "nightowl",
-  theme_toggle = { "nightowl", "one_light" },
+    theme = "catppuccin",
+    theme_toggle = { "catppuccin", "one_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+    hl_override = highlights.override,
+    hl_add = highlights.add,
 
-  tabufline = {
-    overriden_modules = function()
-      return {
-        buttons = function()
-          return ""
+    tabufline = {
+        overriden_modules = function()
+            return {
+                buttons = function()
+                    return ""
+                end,
+            }
         end,
-      }
-    end,
-  },
+    },
 
-  statusline = {
-    seperator_style = "arrow",
-  },
+    statusline = {
+        seperator_style = "arrow",
+    },
 }
 
 M.plugins = "custom.plugins"
