84a3f7fbca2e888b4bf0f4a2a3984fb21ee30d3c
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-15 14:14:50 +0530

    update lsp defaults

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -1,61 +1,63 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "prisma",
-    "rust",
-    "go",
-    "svelte",
-    "astro",
-    "python"
-  },
-  indent = {
-    enable = true,
-  },
+    ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "c",
+        "markdown",
+        "markdown_inline",
+        "prisma",
+        "rust",
+        "go",
+        "svelte",
+        "astro",
+        "python",
+    },
+    indent = {
+        enable = true,
+    },
 }
 
 M.mason = {
-  ensure_installed = {
-    "lua-language-server",
-    "stylua",
-    "rust_analyzer",
-    "rustfmt",
-    "gofumt",
-    "gopls",
-    "go-imports",
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-    "pyright",
-    "autopep8",
-  },
+    ensure_installed = {
+        "lua-language-server",
+        "dockerfile-language-server",
+        "stylua",
+        "rust-analyzer",
+        "rustfmt",
+        "gofumpt",
+        "gopls",
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "svelte-language-server",
+        "prettier_d",
+        "pyright",
+        "autopep8",
+        "eslint-lsp",
+    },
 }
 
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
-
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
+    git = {
+        enable = true,
     },
-  },
+
+    renderer = {
+        highlight_git = true,
+        icons = {
+            show = {
+                git = true,
+            },
+        },
+    },
 }
 
 return M
